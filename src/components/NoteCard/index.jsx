2b0cc1b661e86bb7bf07938abbfcb7d123f1e1a9
Ulhas Daga <ulhasdaga@gmail.com>
2025-09-28 13:34:21 +0530

    crud buttons

--- src/components/NoteCard/index.jsx
+++ src/components/NoteCard/index.jsx
@@ -1,10 +1,19 @@
 import "./index.css"
+import { FaThumbtack } from "react-icons/fa6";
+import { FaThumbtackSlash } from "react-icons/fa6";
+import { FaEdit } from "react-icons/fa";
+import { FaTrash } from "react-icons/fa";
 
 const NoteCard = (props) => {
-    const {title, content, category, timestamp} = props
+    const {id, title, content, category, timestamp, pinned, onTogglePin, onDelete, onEdit} = props
     return (
         <div className="note-card">
-            <h3>{title}</h3>
+            <div>
+                <h3>{title}</h3>
+                <button onClick={() => onTogglePin(id)}>{pinned ? <FaThumbtackSlash /> : <FaThumbtack />}</button>
+                <button onClick={() => onEdit(id)}><FaEdit /></button>
+                <button onClick={() => onDelete(id)}><FaTrash /></button>
+            </div> 
             <p className="note-category">{category}</p>
             <p className="note-content">{content.length > 100 ? content.slice(0,100) + "..." : content}</p>
             {/* <p className="note-timestamp">
